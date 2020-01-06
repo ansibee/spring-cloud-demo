5cd525aebd45aeb53288e778fb84e9079096b33d
ansibee <1250032102@qq.com>
2020-01-06 18:49:23 +0800

    init commit

---
+++ src/test/java/com/zbj/springclouddemo/SpringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbj.springclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
